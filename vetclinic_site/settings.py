f96461594677fc634872266780d9edb0b937cff3
Krotrym <shpakovskey.xx@gmail.com>
2026-05-28 08:52:08 +0300

    Update settings.py

--- vetclinic_site/settings.py
+++ vetclinic_site/settings.py
@@ -138,11 +138,7 @@ STORAGES = {
         'BACKEND': 'django.core.files.storage.FileSystemStorage',
     },
     'staticfiles': {
-        'BACKEND': (
-            'whitenoise.storage.CompressedManifestStaticFilesStorage'
-            if not DEBUG and 'test' not in sys.argv
-            else 'django.contrib.staticfiles.storage.StaticFilesStorage'
-        ),
+        'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage',
     },
 }
 
